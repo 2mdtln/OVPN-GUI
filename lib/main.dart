8e1ef6395658e52d760aefbaaa28878d5265e38f
2mdtln <120330170+2mdtln@users.noreply.github.com>
2024-08-05 10:49:39 +0300

    FileDrop

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
+import 'package:desktop_drop/desktop_drop.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,9 +21,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: const Scaffold(
-        body: Center(child: Text('Hello, World!')),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DropTarget(
+        onDragDone: (details) {
+          _handleDroppedFiles(details.files);
+        },
+        onDragEntered: (details) => debugPrint('Drag entered'),
+        onDragExited: (details) => debugPrint('Drag exited'),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Text(
+              'Drop a .ovpn file to connect',
+              style: Theme.of(context).textTheme.bodySmall,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
       ),
     );
   }
 }
+
+  void _handleDroppedFiles(files) {
+    if (files.isNotEmpty) {
+      for (var file in files) {
+        if (file.path.endsWith('.ovpn')) {
+          debugPrint('Dropped .ovpn file: ${file.path}');
+        } else {
+          debugPrint('Dropped file is not a .ovpn file: ${file.path}');
+        }
+      }
+    } else {
+      debugPrint('No files dropped');
+    }
+  }
